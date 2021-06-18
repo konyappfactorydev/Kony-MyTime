389b016db61523f3687646179d2389e7a33c6930
Dinesh Reddy <dinesh.gioe@gmail.com>
2021-06-18 13:44:29 +0530

    Update testSuite.js

--- testresources/Jasmine/Desktop/Test Suites/testSuite.js
+++ testresources/Jasmine/Desktop/Test Suites/testSuite.js
@@ -4,14 +4,16 @@ define(["Test Cases/testCase1_data"], function() {
 			var _data = testcaseData_1623318108958.dataset;
 			await kony.automation.playback.waitFor(["OauthSamp/Form1","btn"]);
 			// :User Injected Code Snippet [// - [1 lines]]
-			kony.automation.webSocket.connectToHost();
+			await kony.automation.webSocket.connectToHost();
+			await kony.automation.playback.wait(5000);
     		// :End User Injected Code Snippet {2ce691df-2d15-9c25-e1de-cbdf8b1161a5}
 			kony.automation.button.click(["OauthSamp/Form1","btn"]);
 			// :User Injected Code Snippet [// - [2 lines]]
 		    kony.automation.webSocket.sendMessage({ "eventName": "enterUsername"});
+		    await kony.automation.playback.wait(5000);
 		    await kony.automation.webSocket.waitForMessage({ "eventName":"executionDone"});
 		    // :End User Injected Code Snippet {935e8708-19f8-0401-a025-8b89c89d30a9}
 		    expect(kony.automation.widget.getWidgetProperty(["Form1","lbl"], "text")).toEqual(_data.assert.lbl);
-		});
+		}, 90000);
 	});
 });
